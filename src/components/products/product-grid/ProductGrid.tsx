a1aae44aed89bc24c8b64f1ecbccd5eb1db8482d
Daniel Lopez <dafeloru@gmail.com>
2026-04-19 23:54:06 -0500

    feat(ui): update ProductGrid component

--- src/components/products/product-grid/ProductGrid.tsx
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export const ProductGrid = ({ products }: Props) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-20 mb-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8 mb-10">
       {
         products.map(product => (
           <ProductGridItem
