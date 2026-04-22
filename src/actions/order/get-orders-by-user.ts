2c5f64b6d0c27a7491a586bc86d8a0bb3a5782a1
Daniel Lopez <dafeloru@gmail.com>
2026-04-21 21:29:52 -0500

    feat(order): update order actions logic

--- src/actions/order/get-orders-by-user.ts
+++ src/actions/order/get-orders-by-user.ts
@@ -20,6 +20,9 @@ export const getOrdersByUser = async() => {
     where: {
       userId: session.user.id
     },
+    orderBy: {
+      createdAt: "desc"
+    },
     include: {
       OrderAddress: {
         select: {
